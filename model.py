0a2002eba0dfae7c2aca2ca40c377144b85bb76a
Michael Nelson <zsc.michaeln@gmail.com>
2024-05-18 14:04:04 -0500

    Semi-completed project Added functions to model.py Presented work completed

--- model.py
+++ model.py
@@ -166,18 +166,87 @@ def model_testing(X_train,y_train,warn_suppress=True):
     
     return pd.DataFrame(gscv.cv_results_)
 
-def print_report(results,X_set,y_set,n_show=3,suppress_warnings=True):
+# def print_report(results,X_set,y_set,n_show=3,suppress_warnings=True):
+#     """
+#     Shows the top 3 performing algorithms and their classification reports
+    
+#     Parameters:
+#     -----------
+#     - results: DataFrame
+#         A DataFrame containing the cv_results_ of a grid search
+#     - X_set: List
+#         A list containing the 3 DataFrames of X_train, X_validate, and X_test. Does not use the test set.
+#     - y_set: List
+#         A list containing the 3 Series of y_train, y_validate, and y_test. Does not use the test set.
+#     - n_show: Integer, default=3
+#         How many of the top algorithms to show
+#     - suppress_warnings: Boolean, default=True
+#         Whether to suppress warnings or show them.
+    
+#     Returns:
+#     --------
+#     - None
+#     """
+    
+#     # Suppress warnings
+#     if suppress_warnings:
+#         warnings.filterwarnings('ignore')
+#         warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
+    
+#     # Sort by test score
+#     sorted_results_df = results.sort_values('mean_test_score', ascending=False)
+
+#     # Calculate the top 30% cutoff
+#     cutoff_index = int(len(sorted_results_df) * 0.3)
+#     top_30_df = sorted_results_df.head(cutoff_index)
+    
+#     # Extract algorithm names assuming param_clf is directly usable or adjust accordingly
+#     top_30_df['algorithm_name'] = top_30_df['param_clf'].copy().apply(lambda x: type(x).__name__)
+#     top_algorithms = top_30_df['algorithm_name'].value_counts().nlargest(n_show).index.tolist()
+#     print(f"Top {n_show} Algorithm(s):", top_algorithms)
+    
+#     classification_reports = []
+#     for algo in top_algorithms:
+#         # Filter the DataFrame for this specific algorithm
+#         algo_df = top_30_df[top_30_df['algorithm_name'] == algo]
+
+#         # Get the best set of parameters for this algorithm
+#         best_params = algo_df.iloc[0]['params']
+
+#         # Set up the estimator with the best parameters
+#         estimator = gscv.estimator.set_params(**best_params)
+
+#         # Fit the estimator on the full dataset (or a training subset if specified)
+#         estimator.fit(X_set[0], y_set[0])
+
+#         # Optionally, evaluate on a test set and generate classification reports
+#         y_pred = estimator.predict(X_set[1])
+#         report = classification_report(y_set[1], y_pred)
+#         classification_reports.append(report)
+
+#     fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+#     for i, report in enumerate(classification_reports):
+#         axs[i].text(0.01, 1, report, {'fontsize': 10}, fontfamily='monospace', verticalalignment='top', horizontalalignment='left')
+#         axs[i].axis('off')
+#         axs[i].set_title(f"Report for {top_salgorithms[i]}")
+
+#     plt.tight_layout()
+#     plt.show()
+    
+#     return None
+
+def print_report(results, X_set, y_set, n_show=3, suppress_warnings=True):
     """
-    Shows the top 3 performing algorithms and their classification reports
+    Shows the top performing algorithms and their classification reports
     
     Parameters:
     -----------
     - results: DataFrame
         A DataFrame containing the cv_results_ of a grid search
     - X_set: List
-        A list containing the 3 DataFrames of X_train, X_validate, and X_test. Does not use the test set.
+        A list containing the DataFrames of X_train, X_validate, and X_test. Does not use the test set.
     - y_set: List
-        A list containing the 3 Series of y_train, y_validate, and y_test. Does not use the test set.
+        A list containing the Series of y_train, y_validate, and y_test. Does not use the test set.
     - n_show: Integer, default=3
         How many of the top algorithms to show
     - suppress_warnings: Boolean, default=True
@@ -197,38 +266,39 @@ def print_report(results,X_set,y_set,n_show=3,suppress_warnings=True):
     sorted_results_df = results.sort_values('mean_test_score', ascending=False)
 
     # Calculate the top 30% cutoff
-    cutoff_index = int(len(sorted_results_df) * 0.3)
+    cutoff_index = int(len(sorted_results_df) * 0.4)
     top_30_df = sorted_results_df.head(cutoff_index)
     
     # Extract algorithm names assuming param_clf is directly usable or adjust accordingly
-    top_30_df['algorithm_name'] = top_30_df['param_clf'].copy().apply(lambda x: type(x).__name__)
+    top_30_df['algorithm_name'] = top_30_df['param_clf'].apply(lambda x: type(x).__name__)
     top_algorithms = top_30_df['algorithm_name'].value_counts().nlargest(n_show).index.tolist()
-    print(f"Top {n_show} Algorithm(s):", top_algorithms)
+    print(f"Top {len(top_algorithms)} Algorithm(s):", top_algorithms)
     
     classification_reports = []
     for algo in top_algorithms:
         # Filter the DataFrame for this specific algorithm
         algo_df = top_30_df[top_30_df['algorithm_name'] == algo]
 
-        # Get the best set of parameters for this algorithm
-        best_params = algo_df.iloc[0]['params']
-
-        # Set up the estimator with the best parameters
-        estimator = gscv.estimator.set_params(**best_params)
-
+        # Get the index of the best row for this algorithm
+        best_row_index = algo_df.index[0]
+        
+        # Extract the estimator
+        best_estimator = results.loc[best_row_index, 'param_clf']
+        
         # Fit the estimator on the full dataset (or a training subset if specified)
-        estimator.fit(X_set[0], y_set[0])
+        best_estimator.fit(X_set[0], y_set[0])
 
-        # Optionally, evaluate on a test set and generate classification reports
-        y_pred = estimator.predict(X_set[1])
+        # Optionally, evaluate on a validation set and generate classification reports
+        y_pred = best_estimator.predict(X_set[1])
         report = classification_report(y_set[1], y_pred)
         classification_reports.append(report)
 
-    fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+    # Plot the classification reports side by side
+    fig, axs = plt.subplots(1, len(top_algorithms), figsize=(15, 5))
     for i, report in enumerate(classification_reports):
         axs[i].text(0.01, 1, report, {'fontsize': 10}, fontfamily='monospace', verticalalignment='top', horizontalalignment='left')
         axs[i].axis('off')
-        axs[i].set_title(f"Report for {top_salgorithms[i]}")
+        axs[i].set_title(f"Report for {top_algorithms[i]}")
 
     plt.tight_layout()
     plt.show()
@@ -287,7 +357,7 @@ def final_models(X_train,y_train,suppress_warnings=True):
     finalist_pipeline = Pipeline(
         [
             ('scalar',RobustScaler()),
-            ('kbest',fselect.SelectKBest()),
+            ('kbest',SelectKBest()),
             ('clf',MLPClassifier())
         ]
     )
